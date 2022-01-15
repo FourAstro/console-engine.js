e6545910afbb55b93161b9c3f3e245d7afe68948
Cleetus <rubenjafethdebruijne@gmail.com>
2022-01-15 15:46:59 +0100

    0.0.3-dev progress

--- controller.js
+++ controller.js
@@ -1,5 +1,6 @@
 const child_process = require("child_process")
 const prompt = require("prompt")
+const matrix_js = require("matrix-js")
 
 var sessionConfig = {
     in: null,
@@ -8,33 +9,68 @@ var sessionConfig = {
     rows: null,
 }
 
+var matrix
+
+/**
+ *     COLUMN - COLUMN - COLUMN - COLUMN - COLUMN - COLUMN
+ * ROW
+ * ROW
+ * ROW
+ * ROW
+ * ROW
+ */
+
 module.exports = {
     /**
      * Intiate console-engine.js
      * @param {*} stdin 
      * @param {*} stdout 
-     * @returns {Object} { columns: int, rows: int }
+     * @param {Boolean} calibrate If you would like a animation.
+     * @returns {Object} `{ columns: int, rows: int }` Columns are lines vertically from left to right, Rows are horizontal loines from top to bottom.
      */
-    config: function (stdin, stdout) {
+    config: function (stdin, stdout, calibrate, returnData) {
         console.clear()
 
         if (!stdin || !stdout) {
-            return console.log(new TypeError("Missing parameters, either 'stdin' or 'stdout'"))
+            return console.error(new TypeError("Missing parameters, either 'stdin' or 'stdout'"))
+        }
+
+        if (typeof calibrate != Boolean) {
+            return console.error(new TypeError("Missing parameters, 'calibrate' is not a boolean"))
         }
 
         sessionConfig.in = stdin
         sessionConfig.out = stdout
 
         if (!sessionConfig.out.columns || !sessionConfig.out.rows) {
-            return console.log(new TypeError("'stdout' does not have either 'columns' or 'rows' parameter"))
+            return console.error(new TypeError("'stdout' does not have either 'columns' or 'rows' parameter"))
         }
 
         sessionConfig.columns = sessionConfig.out.columns
         sessionConfig.rows = sessionConfig.out.rows
 
+        matrix = matrix_js([sessionConfig.columns, sessionConfig.rows])
+
         return {
             columns: sessionConfig.columns,
-            rows: sessionConfig.rows
+            rows: sessionConfig.rows,
+            matrix: matrix
         };
+    },
+
+    sessionConfig: sessionConfig,
+
+    /**
+     * Center 1 line of string (text)
+     * @param {String} string 
+     */
+    center: function (string) {
+        if (!string) {
+            return console.error(new TypeError("Missing 'string' parameter"))
+        }
+
+        var col = Math.floor(sessionConfig.columns / 2)
+        var row = Math.floor(sessionConfig.rows / 2)
+        var startCol = col - string.length
     }
 }
